60e0de8f6541e6bbdb59718dbc91a1722e265568
imhalid <imhalid@icloud.com>
2022-03-14 19:23:25 +0300

    this project is completed

--- script.js
+++ script.js
@@ -4,22 +4,29 @@ let length = document.getElementById("length");
 let volume = document.getElementById("volume");
 let mass = document.getElementById("mass");
 
+let inputValue = document.getElementById("tentacles");
+
+function result() {
+  console.log(inputValue.value);
+}
+
 function generator() {
+  value = inputValue.value;
   //length
   // 20 meters = 65.616 feet | 20 feet = 6.096 meters
 
-  let meterstToFeet = value.innerText * 3.281;
-  let feetToMeters = value.innerText / 3.282;
+  let meterstToFeet = value * 3.281;
+  let feetToMeters = value / 3.282;
   feetToMeters = feetToMeters.toFixed(3);
   meterstToFeet = meterstToFeet.toFixed(3);
 
   length.innerText =
-    value.innerText +
+    value +
     " meters = " +
     meterstToFeet +
     " feet" +
     " | " +
-    value.innerText +
+    value +
     " feet = " +
     feetToMeters +
     " meters";
@@ -27,19 +34,19 @@ function generator() {
   //volume
   //20 meters = 65.616 feet | 20 feet = 6.096 meters / 0.264172
 
-  let litersToGallons = value.innerText / 0.264172;
-  let gallonstoLiters = value.innerText * 0.264172;
+  let litersToGallons = value / 0.264172;
+  let gallonstoLiters = value * 0.264172;
 
   litersToGallons = litersToGallons.toFixed(3);
   gallonstoLiters = gallonstoLiters.toFixed(3);
 
   volume.innerText =
-    value.innerText +
+    value +
     " liters = " +
     litersToGallons +
     " gallons" +
     " | " +
-    value.innerText +
+    value +
     " gallons = " +
     gallonstoLiters +
     " liters";
@@ -47,18 +54,18 @@ function generator() {
   //mass
   //20 meters = 65.616 feet | 20 feet = 6.096 meters / 2.205
 
-  let kilogramsToPounds = value.innerText / 2.205;
-  let poundsToKilograms = value.innerText * 2.205;
+  let kilogramsToPounds = value / 2.205;
+  let poundsToKilograms = value * 2.205;
   kilogramsToPounds = kilogramsToPounds.toFixed(3);
   poundsToKilograms = poundsToKilograms.toFixed(3);
 
   mass.innerText =
-    value.innerText +
+    value +
     " kilograms = " +
     kilogramsToPounds +
     " pounds" +
     " | " +
-    value.innerText +
+    value +
     " pounds = " +
     poundsToKilograms +
     " kilograms";
